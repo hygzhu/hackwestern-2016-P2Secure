e8419018d35adbc2ccf5e05f708eb8a36f4d2d8d
Arthur <contactaguo@gmail.com>
2016-10-15 23:21:27 -0400

    add products endpoints

---
+++ BorrowIt/models/product.database.model.ts
@@ -0,0 +1,47 @@
+import {Mongoose} from 'mongoose';
+
+var mongoose: Mongoose = require('mongoose');
+//mongoose.connect('mongodb://harman666666:123456@ds061454.mlab.com:61454/borrowit'); 
+var Schema = mongoose.Schema;
+
+//Custom Validators: 
+
+var productNameValidator = [
+    function(val: string) {
+        return (val.length > 0 && val.toLocaleLowerCase() != 'none');
+    },
+    //Custom error text...
+    'Select a valid user name.' ];
+
+var productSchema = new Schema({
+    productName: {
+        type: String,
+        required: true,
+        validate: productNameValidator
+    },
+    collateralAmount: {
+        type: int,
+        required: true, 
+    },
+    borrowingFeeAmount: {
+        type: String,
+        required: true
+    } , 
+    dueDate: {
+        type: String,
+        required: true
+    }, borrowerWallet:{
+        type: String,
+        required: false,
+        default: ''
+    }, lenderWallet: {
+        type: String,
+        required: false
+        //validate: emailValidator
+    }
+});
+
+
+// Expose (export) the model now...
+var Product = mongoose.model('Product', productSchema);
+module.exports = Product;
---
+++ BorrowIt/models/product.server.model.ts
@@ -0,0 +1,28 @@
+
+
+export class UserModel {
+
+        productName: string;
+        collateralAmount: number;
+        borrowingFeeAmount: number;
+        dueDate: string;
+        borrowerWallet: string;
+        lenderWallet: string;
+
+constructor(
+        productName: string,
+        collateralAmount: number,
+        borrowingFeeAmount: number,
+        dueDate: string,
+        borrowerWallet: string,
+        lenderWallet: string
+       ) {
+        this.productName = productName;
+        this.collateralAmount = collateralAmount ;
+        this.borrowingFeeAmount = borrowingFeeAmount;
+        this.dueDate= dueDate;
+        this.borrowerWallet= borrowerWallet;
+        this.lenderWallet = lenderWallet;
+    }
+
+} 
